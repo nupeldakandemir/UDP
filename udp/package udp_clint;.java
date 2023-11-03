ad42df3a937d91cf61eb3b2ad7c72e43959c41b8
Nupelda Kandemir <120253252+nupeldakandemir@users.noreply.github.com>
2023-11-03 23:10:13 +0300

    Add files via upload

---
+++ udp/package udp_clint;.java
@@ -0,0 +1,25 @@
+package udp_clint;
+import java.io.*;
+import java.net.*;
+public class udp_clint {
+	
+
+	   public static void main(String args[]) throws Exception
+	   {
+	      BufferedReader inFromUser =
+	         new BufferedReader(new InputStreamReader(System.in));
+	      DatagramSocket clientSocket = new DatagramSocket();
+	      InetAddress IPAddress = InetAddress.getByName("192.168.170.102"); //server adresi
+	      byte[] sendData = new byte[1024];
+	      byte[] receiveData = new byte[1024];
+	      String sentence = inFromUser.readLine();
+	      sendData = sentence.getBytes();
+	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); //server port numarası 9876
+	      clientSocket.send(sendPacket);
+	      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	      clientSocket.receive(receivePacket);
+	      String modifiedSentence = new String(receivePacket.getData());
+	      System.out.println("FROM SERVER:" + modifiedSentence);
+	      clientSocket.close();
+	   }
+	}
