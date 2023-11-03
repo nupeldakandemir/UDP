c11e6fdd5b48e8dec83b99a3cdb02f78e220d222
Nupelda Kandemir <120253252+nupeldakandemir@users.noreply.github.com>
2023-11-03 23:43:36 +0300

    Update package udp_clint;.java

--- udp/package udp_clint;.java
+++ udp/package udp_clint;.java
@@ -9,12 +9,12 @@ public class udp_clint {
 	      BufferedReader inFromUser =
 	         new BufferedReader(new InputStreamReader(System.in));
 	      DatagramSocket clientSocket = new DatagramSocket();
-	      InetAddress IPAddress = InetAddress.getByName("192.168.170.102"); //server adresi
+	      InetAddress IPAddress = InetAddress.getByName("localhost"); //write server address
 	      byte[] sendData = new byte[1024];
 	      byte[] receiveData = new byte[1024];
 	      String sentence = inFromUser.readLine();
 	      sendData = sentence.getBytes();
-	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); //server port numarası 9876
+	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); //server port number 9876
 	      clientSocket.send(sendPacket);
 	      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 	      clientSocket.receive(receivePacket);
